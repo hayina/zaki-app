e0fb5978894e38834347cce71aacdf8d72c89ee6
youssef <youssef.lechqar@gmail.com>
2018-02-13 18:21:34 +0000

    Structure OK

---
+++ src/components/Layout.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+export default class Layout extends React.Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, Lam!</h1>
+            </div>
+        )
+    }
+
+}
+
